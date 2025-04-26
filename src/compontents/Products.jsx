9d5826401e3a1b1074a90f2d50e0f7848a382938
Krzysztof Mazur <mazurkrzysztof377@example.com~>
2025-04-26 09:47:46 +0200

    Add loader icon from React Spinner

--- src/compontents/Products.jsx
+++ src/compontents/Products.jsx
@@ -5,6 +5,7 @@ import { useEffect } from "react";
 import { fetchCategories, fetchProducts } from "../store/actions";
 import Filter from "./Filter";
 import useProductFilter from "./useProductFilter";
+import Loader from "./Loader";
 
 const Products = () => {
     
@@ -26,8 +27,9 @@ const Products = () => {
         <div className="lg:px-14 sm:px-8 px-4 py-14 2xl:w-[90%] 2xl:mx-auto">
             <Filter categories = {categories ? categories : []}/>
             {isLoading ? (
-                <p>It is loading...</p>
-            ) : errorMessage ? (
+                    <Loader />
+                ) : errorMessage ? (
+
                 <div className="flex justify-center items-center h-[200px]">
                     <FaExclamationTriangle className="text-slate-800 text-3xl mr-2"/>
                     <span className="text-slate-800 text-lg font-medium">
