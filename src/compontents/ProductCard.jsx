34dd5b2d0bd3894473657620e1ed10a11735338a
Krzysztof Mazur <mazurkrzysztof377@example.com~>
2025-05-03 16:19:18 +0200

    Fix description display

--- src/compontents/ProductCard.jsx
+++ src/compontents/ProductCard.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { FaShoppingCart } from "react-icons/fa";
 import ProductViewModal from "./ProductViewModal";
+import truncateText from "./truncateText";
 
 const ProductCard = ({
             productId,
@@ -63,7 +64,7 @@ const ProductCard = ({
                 </h2>
 
                 <div className="min-h-20 max-h-20 text-left">
-                    <p className="text-gray-600 text-sm">{description}</p>
+                    <p className="text-gray-600 text-sm">{truncateText(description,80)}</p>
                 </div>
 
 
