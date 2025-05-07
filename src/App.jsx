24ae4e4333454ecab2df45ec9e64fa1763f7bd91
Krzysztof Mazur <mazurkrzysztof377@example.com~>
2025-05-07 14:26:54 +0200

    Add Routing

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,19 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Products from './compontents/Products'
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from './compontents/home/Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <Products />
-    </>
+    <Router>
+      <Routes>
+        <Route path='/' element={ <Home />}/>
+        <Route path='/products' element={ <Products />}/>
+      </Routes>
+    </Router>
   )
 }
 
